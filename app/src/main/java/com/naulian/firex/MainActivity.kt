cb36e134e47bc0312bd629ddccee4d1e8683cee2
Naulian <cinkhangin@gmail.com>
2023-05-07 21:26:14 +0630

    release 1.0.0

--- app/src/main/java/com/naulian/firex/MainActivity.kt
+++ app/src/main/java/com/naulian/firex/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.naulian.firex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
